e1f6011cd2a91db1399fad382356ae7598d48a91
ryanBrittes <87885748+RyanBrittes@users.noreply.github.com>
2025-09-11 18:27:15 +0000

    update

---
+++ FirstReverse.py
@@ -0,0 +1,20 @@
+#Challenge: Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order. For example: if the input string is "Hello World and Coders" then your program should return the string sredoC dna dlroW olleH.
+
+#Example
+#Input: "Coderbyte"
+#Output: "etybredoC"
+
+#---------------------------------------------------------------------------------------------------
+
+def FirstReverse(strParam):
+  word = ""
+  i = len(strParam) - 1
+  
+  while i != -1:
+    word += strParam[i]
+    i -= 1
+
+  return word
+
+# keep this function call here 
+print(FirstReverse(input("Text here --> ")))
